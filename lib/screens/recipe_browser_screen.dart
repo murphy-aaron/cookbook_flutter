5e7e570c61827751ff202321acfeda38c27b62b9
Aaron Murphy <aamurphy98@gmail.com>
2024-06-30 21:20:04 -0400

    created recipe browser screen

---
+++ lib/screens/recipe_browser_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeBrowserScreen extends StatelessWidget {
+  const RecipeBrowserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recipe Browser'),
+      ),
+      body: Container(),
+    );
+  }
+}
