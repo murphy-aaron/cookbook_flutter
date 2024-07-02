c3b2e2349be2a97a00215fa548d7badfeba220ff
Aaron Murphy <aamurphy98@gmail.com>
2024-07-01 20:23:00 -0400

    Added recipe image and title to recipe screen

--- lib/screens/recipe_screen.dart
+++ lib/screens/recipe_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cookbook_flutter/components/recipe_image.dart';
 import 'package:flutter/material.dart';
 
 class RecipeScreen extends StatelessWidget {
@@ -11,6 +12,31 @@ class RecipeScreen extends StatelessWidget {
       backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
         backgroundColor: Colors.grey.shade100,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 180.0,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: NetworkImage(
+                  'https://assets.epicurious.com/photos/5988e3458e3ab375fe3c0caf/1:1/w_1280,c_limit/How-to-Make-Chicken-Alfredo-Pasta-hero-02082017.jpg'),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Text(
+                  'Recipe Title Goes Here',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
